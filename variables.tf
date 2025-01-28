778027022c22c1d8d3d4254c234faf4bb38e1efd
ganesh20101 <115871700+ganesh20101@users.noreply.github.com>
2025-01-28 16:19:19 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
